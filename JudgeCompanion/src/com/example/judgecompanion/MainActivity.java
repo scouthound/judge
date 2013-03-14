192f635375c13e5cf85a9fb55b04e179540816f6
David Solares <phantomzwei@gmail.com>
2013-03-13 20:37:02 -0400

    Set up basic transition to server activity.

--- JudgeCompanion/src/com/example/judgecompanion/MainActivity.java
+++ JudgeCompanion/src/com/example/judgecompanion/MainActivity.java
@@ -2,16 +2,21 @@ package com.example.judgecompanion;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // If in portrait, use landscape layout...
         if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
         	setContentView(R.layout.activity_main_landscape);
+        // Else use portrait layout
         else
         	setContentView(R.layout.activity_main_portrait);  
     }
@@ -23,4 +28,31 @@ public class MainActivity extends Activity {
         return true;
     }
     
+    public void createEvent(View theView)
+    {
+    	Toast.makeText(this.getApplicationContext(), "Creating Competition...", Toast.LENGTH_SHORT).show();
+    	// buildTables();
+    	// Intent i = create something
+    	// startActivity(i);
+    }
+    
+    public void joinEvent(View theView)
+    {
+    	Toast.makeText(this.getApplicationContext(), "Joining Competition...", Toast.LENGTH_SHORT).show();
+    	String competition = authenticateUser("userID");
+    	Intent intent;
+    	
+		if(true) // if a valid event is found, join it
+		{
+			Toast.makeText(this.getApplicationContext(), "Joined Competition ID-" + competition, Toast.LENGTH_SHORT).show();
+			intent = new Intent(this, ServerActivity.class);
+			startActivity(intent);
+		}
+    }
+    
+    // Dummy method: returns competition id
+    private String authenticateUser(String userId)
+    {
+    	return "1234";
+    }
 }
